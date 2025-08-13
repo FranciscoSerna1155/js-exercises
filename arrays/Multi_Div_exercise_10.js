35afa96eefc21b9b0d34dfac15d98074da68cb90
vboxuser <vboxuser@vbox.myguest.virtualbox.org>
2025-08-12 21:13:13 -0600

    Codigos Corregidos

--- arrays/Multi_Div_exercise_10.js
+++ arrays/Multi_Div_exercise_10.js
@@ -6,20 +6,20 @@ un boton este procesa a  hacer la respectiva multiplicacion o division,
 y depues otra para mostrar el texto en una etiqueta 
 */
 
-function get_Number(){
+function multiplyBy(){
 //obtiene los valores de la entrada  el primero y segundo numero 
-	num1 = document.getElementById("firstNumber").value;
-	num2 = document.getELementById("secondNumber").value;
+	num1 = parseFloat(document.getElementById("firstNumber").value);
+	num2 = parseFloat(document.getELementById("secondNumber").value);
 //activa el poder ver el producto de los dos elemtos
-	document.getELementbyId("result").innerHTML = num1 * num2;
+	document.getElementbyId("result").innerHTML = num1 * num2;
 
 }
 
 
 function divideBy() {
 
-	num1 = document.getELementById("firstNumber").value;
-	num2 = document.getELementById("secondNumber").value;
+	num1 = document.getElementById("firstNumber").value;
+	num2 = document.getElementById("secondNumber").value;
 
-	document.getELementbyIdy("result").innerHTML = num1 / num2;
+	document.getElementbyId("result").innerHTML = num1 / num2;
 }
